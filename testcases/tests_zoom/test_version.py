4825194f43da0dd5e21f65b379de5d5586f4b84e
Saddala <chandini.saddala@capgemini.com>
2025-09-18 15:41:05 +0530

    modified1

--- testcases/tests_zoom/test_version.py
+++ testcases/tests_zoom/test_version.py
@@ -2,13 +2,15 @@
 
 from modules.version import get_collabos_version, get_collab_version_from_adb
 import utils as util
+from utils import get_selected_device
 
 
 util.have_auth()
 def test_version_verification():
     """Test to verify the software version displayed on the web page matches the device version."""
     web_version = get_collabos_version()
-    device_version = get_collab_version_from_adb("10.91.231.25")
+    device=get_selected_device()
+    device_version = get_collab_version_from_adb(device)
 
     assert device_version is not None, "Failed to retrieve version from device."
     assert web_version is not None, "Failed to extract version from web page."
